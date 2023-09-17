d5a8d1a7b854b179366201eb18b8404e092461d5
wpgosocial <romeoclen3@gmail.com>
2023-09-17 06:28:24 -0400

    RouterAddress

--- BlackPaperToken/migrations/1_deploy_blackPaperToken.js
+++ BlackPaperToken/migrations/1_deploy_blackPaperToken.js
@@ -3,7 +3,9 @@ const BlackPaperToken = artifacts.require("BlackPaperToken");
 module.exports = function(deployer, network, accounts) {
     // For simplicity, let's use the first account as the deployer
     const deployerAccount = accounts[0];
-    const uniswapRouterAddress = "0xB26B2De65D07eBB5E54C7F6282424D3be670E1f0";
+    //const uniswapRouterAddress = "0xB26B2De65D07eBB5E54C7F6282424D3be670E1f0";
+
+    const uniswapRouterAddress = "0x86dcd3293C53Cf8EFd7303B57beb2a3F671dDE98";
  
     deployer.deploy(BlackPaperToken, uniswapRouterAddress, { from: deployerAccount });
 };
