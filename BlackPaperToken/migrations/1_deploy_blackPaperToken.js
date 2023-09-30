8fe926dab8ee4aaf0bdd41dc703f30a39c52fc12
wpgosocial <romeoclen3@gmail.com>
2023-09-29 23:08:52 -0400

    dependence update

--- BlackPaperToken/migrations/1_deploy_blackPaperToken.js
+++ BlackPaperToken/migrations/1_deploy_blackPaperToken.js
@@ -3,7 +3,8 @@ const BlackPaperToken = artifacts.require("BlackPaperToken");
 module.exports = function(deployer, network, accounts) {
     // For simplicity, let's use the first account as the deployer
     const deployerAccount = accounts[0];
-    const uniswapRouterAddress = "0xB26B2De65D07eBB5E54C7F6282424D3be670E1f0";
-    //const uniswapRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+
+    const uniswapRouterAddress = "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD"; //Sepolia address from the unniswap team
+ 
     deployer.deploy(BlackPaperToken, uniswapRouterAddress, { from: deployerAccount });
 };
